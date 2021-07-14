f3d97d5c56c303d37f332104a6ddf8eeca6fbd45
Sonic Squirrels Programming <sonicsquirrels@gmail.com>
2021-07-14 15:52:42 -0700

    Correction to the motor IDs

--- src/main/java/org/frcteam2910/mk3/Constants.java
+++ src/main/java/org/frcteam2910/mk3/Constants.java
@@ -1,20 +1,20 @@
 package org.frcteam2910.mk3;
 
 public class Constants {
-    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 22;
-    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 27;
-    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 32;
-    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 37;
+    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1;
+    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 2;
+    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 3;
+    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 4;
 
-    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 21;
-    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 26;
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 31;
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 36;
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 11;
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 12;
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 13;
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 14;
 
-    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 23;
-    public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 28;
-    public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 33;
-    public static final int DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 38;
+    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 21;
+    public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 22;
+    public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 23;
+    public static final int DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 24;
 
     public static final int PIGEON_PORT = 51;
 
